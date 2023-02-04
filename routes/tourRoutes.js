3d4e9957b48913d72dbb66f87d5040b2414c81e0
Himanshu <hv65616@gmail.com>
2023-02-04 21:06:25 +0530

    including param middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const toursrouter = express.Router();
 const tourcontroller = require('../controller/tourController');
+// param midlleware
+toursrouter.param('id', tourcontroller.checkid);
 toursrouter
   .route('/')
   .get(tourcontroller.getalltours)
