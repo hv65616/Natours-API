37d909ffd31abcd68f3c7f74b13491c299c4f94e
Himanshu <hv65616@gmail.com>
2023-04-04 22:11:13 +0530

    setting up mongodb with our application

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
-const port = 3000;
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+const db = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+// console.log(app.get('env'));
+// console.log(process.env);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // The below code shows all connections request and messages and not necessary to console log
+    // console.log(con.connection);
+    console.log('DB connection successful');
+  });
+
 app.listen(port, () => {
   console.log('Server is listening on port 3000');
 });
+
+// mongodb+srv://vermah564:<password>@cluster0.qpawwas.mongodb.net/Natours?retryWrites=true&w=majority
+
+// npm run start:prod or start:dev
