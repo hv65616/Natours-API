b513d3574e53de1509b20d6eaea6addb10ada680
Himanshu <hv65616@gmail.com>
2023-05-07 00:53:43 +0530

    adding data to online mongodb atlas

--- server.js
+++ server.js
@@ -22,37 +22,17 @@ mongoose
     console.log('DB connection successful');
   });
 
-// This is the schema for  the tour accoding to which we have to provide data for the time it is temporary and will be updated in future if required
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// We have included our tourschema into tour and this will be used for providing data to the mongodb database
-const Tour = mongoose.model('Tour', tourSchema);
-
 // Below code is used 
-const testTour = new Tour({
-  name: 'The Park Campher',
-  rating: 4.7,
-  price: 497,
-});
-testTour.save().then(doc=>{
-  console.log(doc);
-}).catch(err=>{
-  console.log(err);
-})
+// const testTour = new Tour({
+//   name: 'The Park Campher',
+//   rating: 4.7,
+//   price: 497,
+// });
+// testTour.save().then(doc=>{
+//   console.log(doc);
+// }).catch(err=>{
+//   console.log(err);
+// })
 
 app.listen(port, () => {
   console.log('Server is listening on port 3000');
