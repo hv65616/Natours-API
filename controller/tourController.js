ee95e0e0f7bded9d803d35fd188203ec49209ee0
Himanshu <hv65616@gmail.com>
2023-06-30 00:47:26 +0530

    deleting a particular tour

--- controller/tourController.js
+++ controller/tourController.js
@@ -134,8 +134,20 @@ const updatetour = async (req, res) => {
   }
 };
 
-const deletetour = (req, res) => {
-  res.status(204).json({ status: 'success', data: null });
+const deletetour = async (req, res) => {
+  try {
+    const tourdelete = await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: tourdelete,
+      message: 'Particular tour has been successfully deleted',
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
 };
 
 module.exports = {
