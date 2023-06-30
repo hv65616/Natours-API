880ead74ab102b2cd904fafce9276237372fb0d5
Himanshu <hv65616@gmail.com>
2023-06-30 21:01:47 +0530

    adding more schema in tour models for handling more data

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,21 +1,67 @@
 const mongoose = require('mongoose');
-// This is the schema for  the tour accoding to which we have to provide data for the time it is temporary and will be updated in future if required
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  pricediscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdate: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
-
-// We have included our tourschema into tour and this will be used for providing data to the mongodb database
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
+
+// trim remove all the whitespace from starting and from end
+
+// This is the schema for  the tour accoding to which we have to provide data for the time it is temporary and will be updated in future if required
+
+// Its better to define variable names as you are using in passing in from json format so as to avoid any data to get missed
+
+// If you have set required as true and you are passing differnt variable name differnet in model and different in passing data json if a change of lower and upper is there it will show error
+
+// We have included our tourschema into tour and this will be used for providing data to the mongodb database
