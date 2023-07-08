0b6da7657240488a501aca3065a1af3ca59c590b
Himanshu <hv65616@gmail.com>
2023-07-08 17:16:22 +0530

    implementing custom simple error handling middleware for invalid routes

--- app.js
+++ app.js
@@ -33,9 +33,24 @@ app.use('/api/v1/user', userrouter);
 
 // route for all invalid request and should always put atlast
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+// Simpel error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
