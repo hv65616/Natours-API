5aef33cc0ad63c37e16ea7782da05762b29ebf31
Himanshu <hv65616@gmail.com>
2023-07-29 21:52:54 +0530

    handlecasterror function create

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,5 @@
+const appError = require('../utils/appError');
+
 const senderrorfordev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -23,12 +25,21 @@ const senderrorforprod = (err, res) => {
     });
   }
 };
+// This function is responsible to handle error of our mongoose
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new appError(message, 400);
+};
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
     senderrorfordev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    senderrorforprod(err, res);
+    let error = { ...err };
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    senderrorforprod(error, res);
   }
 };
