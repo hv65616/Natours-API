e4fe000619058b3ed53bc3ddf51c0dd7be4a3123
Himanshu <hv65616@gmail.com>
2023-08-02 22:22:30 +0530

    adding custom validator and custom middlewaret to encrypt password

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,7 @@
 // We are also using validator to validate the email provided by the user is correct or not
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -25,8 +26,25 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on save
+      // Its a custom validator which is used to validate the password
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password and Password confirm does not match',
+    },
   },
 });
-
+// this middle ware function is used to encrypt the password before saving it into the database
+userSchema.pre('save', async function (next) {
+  // Only run this function if password is actually modified
+  if (!this.isModified('password')) return next();
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Does not store the passwordconfirm into the database
+  this.passwordConfirm = undefined;
+  next();
+});
 const User = mongoose.model('User', userSchema);
 module.exports = User;
