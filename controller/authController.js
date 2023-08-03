6d3c27074e81448fc6d3255f093675af15180e55
Himanshu <hv65616@gmail.com>
2023-08-03 21:30:10 +0530

    jwt auth token created using id of user data and later store in databse

--- controller/authController.js
+++ controller/authController.js
@@ -2,10 +2,23 @@
 // It basically check the details provided by the user is valid and correct to be stored into the database
 const User = require('../models/userModel');
 const catchasync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 const signup = catchasync(async (req, res, next) => {
-  const newuser = await User.create(req.body);
+  // This piece of code is wrong as it store all the data and anyone can access the data by registerign themselves as admin
+  // const newuser = await User.create(req.body);
+  const newuser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  // JWT Auth token created which is using id of the user and later storing that token into the database
+  const token = jwt.sign({ id: newuser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'Success',
+    token,
     data: {
       user: newuser,
     },
