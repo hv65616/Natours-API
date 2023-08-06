59ebaafb711a1fee048680f86ab34a3dff90d478
Himanshu <hv65616@gmail.com>
2023-08-06 18:43:16 +0530

    login route and comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,16 @@ const userrouter = express.Router();
 const usercontroller = require('../controller/userController');
 // Adding the route for the creating user
 const authcontroller = require('../controller/authController');
+// route for signup
 userrouter.post('/signup', authcontroller.signup);
-
+// route for login
+userrouter.post('/login', authcontroller.login);
+// route for getallusers and createusers
 userrouter
   .route('/')
   .get(usercontroller.getallusers)
   .post(usercontroller.creatuser);
+// route for particular user,update user and delete user
 userrouter
   .route('/:id')
   .get(usercontroller.getuser)
