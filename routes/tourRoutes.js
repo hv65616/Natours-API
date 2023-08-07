c9126d1bff580382226e9e9e0621870c04a025fd
Himanshu <hv65616@gmail.com>
2023-08-08 00:33:50 +0530

    authorization verification for getting all tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const toursrouter = express.Router();
 const tourcontroller = require('../controller/tourController');
+const authcontroller = require('../controller/authController');
 // param midlleware
 // toursrouter.param('id', tourcontroller.checkid);
 
@@ -12,7 +13,7 @@ toursrouter.route('/tour-stats').get(tourcontroller.gettourstats);
 toursrouter.route('/monthly-plan/:year').get(tourcontroller.getmonthlyplans);
 toursrouter
   .route('/')
-  .get(tourcontroller.getalltours)
+  .get(authcontroller.protect, tourcontroller.getalltours)
   .post(tourcontroller.createnewtour);
 toursrouter
   .route('/:id')
