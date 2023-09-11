8556f49d668d4f4b9b9f4d7ecdeb6589e10348ab
Himanshu <hv65616@gmail.com>
2023-09-12 01:41:43 +0530

    review model added

---
+++ models/reviewModels.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A tour must have a review'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'A tour must have a rating'],
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.'],
+    },
+  },
+  //   the use of this virtual property it to show data or the fields when we are not storing any field directly into database
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
