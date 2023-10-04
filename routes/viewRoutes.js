cad170e89b156d2a230c5f3d6244d1906a1b030f
Himanshu <hv65616@gmail.com>
2023-10-04 23:19:17 +0530

    routes setup

---
+++ routes/viewRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const viewcontroller = require('../controller/viewController');
+// Overview route to get all tour
+router.get('/overview', viewcontroller.getoverview);
+// Tour route to get particular tour
+router.get('/tour', viewcontroller.gettour);
+module.exports = router;
