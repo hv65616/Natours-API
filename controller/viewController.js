6467fff18a8a44d50a3b7f1b9bca917533d55430
Himanshu <hv65616@gmail.com>
2023-10-05 23:19:58 +0530

    tours exported

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,11 +1,18 @@
-const getoverview = (req, res) => {
+const Tour = require('../models/tourModels');
+const catchasync = require('../utils/catchAsync');
+const getoverview = catchasync(async (req, res, next) => {
+  // 1. Get tour data from collection
+  const tours = await Tour.find();
+  // 2. Build  template
+  // 3. Render that template using the tour data
   res.status(200).render('overview', {
     title: 'All tours',
+    tours
   });
-};
+});
 const gettour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forset Hiker',
   });
 };
-module.exports = { getoverview,gettour };
+module.exports = { getoverview, gettour };
