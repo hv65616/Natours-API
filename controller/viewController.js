963b76b1864847396722c2bc1c9ad67359b1673e
Himanshu <hv65616@gmail.com>
2023-10-09 16:05:22 +0530

    reqesting data and showing it complete

--- controller/viewController.js
+++ controller/viewController.js
@@ -7,12 +7,20 @@ const getoverview = catchasync(async (req, res, next) => {
   // 3. Render that template using the tour data
   res.status(200).render('overview', {
     title: 'All tours',
-    tours
+    tours,
   });
 });
-const gettour = (req, res) => {
+const gettour = async (req, res) => {
+  // 1. get data for the requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2.Build template
+  // 3. Render the template using data from step 1
   res.status(200).render('tour', {
-    title: 'The Forset Hiker',
+    title: `${tour.name} Tour`,
+    tour,
   });
 };
 module.exports = { getoverview, gettour };
