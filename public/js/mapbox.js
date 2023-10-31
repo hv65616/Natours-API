d433b9e7d18108a32d7c74cf5d5d44db4235cefc
Himanshu <hv65616@gmail.com>
2023-10-31 14:38:09 +0530

    writing the script for map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,26 @@
 console.log('Hello from client side');
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+var map = L.map('map', { zoomControl: true });
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  crossOrigin: '',
+}).addTo(map);
+
+const points = [];
+locations.forEach((loc) => {
+  points.push([loc.coordinates[1], loc.coordinates[0]]);
+  L.marker([loc.coordinates[1], loc.coordinates[0]])
+    .addTo(map)
+    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+      autoClose: false,
+    })
+    .openPopup();
+});
+
+const bounds = L.latLngBounds(points).pad(0.5);
+map.fitBounds(bounds);
+
+map.scrollWheelZoom.disable();
