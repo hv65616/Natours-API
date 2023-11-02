2c201818454109b9bc8262a03c8fad03c8103086
Himanshu <hv65616@gmail.com>
2023-11-02 15:02:37 +0530

    include global isloggedin middleware

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const viewcontroller = require('../controller/viewController');
+const authcontroller = require('../controller/authController');
+router.use(authcontroller.isloggedin);
 // Overview route to get all tour
 router.get('/overview', viewcontroller.getoverview);
 // Tour route to get particular tour
