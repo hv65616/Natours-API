a758667104d3276cdf26405c85f6c8fc7599eb19
Himanshu <hv65616@gmail.com>
2023-11-04 19:39:41 +0530

    only loggedin middleware in tour

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const viewcontroller = require('../controller/viewController');
 const authcontroller = require('../controller/authController');
-router.use(authcontroller.isloggedin);
+// router.use(authcontroller.isloggedin);
 // Overview route to get all tour
 router.get('/overview', viewcontroller.getoverview);
 // Tour route to get particular tour
-// router.get('/tour/:slug', authcontroller.protect, viewcontroller.gettour);
-router.get('/tour/:slug', viewcontroller.gettour);
+router.get('/tour/:slug', authcontroller.protect, viewcontroller.gettour);
+// router.get('/tour/:slug',viewcontroller.gettour);
 router.get('/login', viewcontroller.getloginform);
 module.exports = router;
