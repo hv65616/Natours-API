d1025ab37e97bab77909f0705b0020c254770164
Himanshu <hv65616@gmail.com>
2023-11-08 15:27:14 +0530

    tour not found error handle

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModels');
 const catchasync = require('../utils/catchAsync');
+const apperror = require('../utils/appError');
 const getoverview = catchasync(async (req, res, next) => {
   // 1. Get tour data from collection
   const tours = await Tour.find();
@@ -10,12 +11,17 @@ const getoverview = catchasync(async (req, res, next) => {
     tours,
   });
 });
-const gettour = catchasync(async (req, res) => {
+const gettour = catchasync(async (req, res,next) => {
   // 1. get data for the requested tour(including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
+  // console.log(tour);
+  if (!tour) {
+    // console.log("Not found");
+    return next(new apperror('There is no tour with that name', 404));
+  }
   // 2.Build template
   // 3. Render the template using data from step 1
   res.status(200).render('tour', {
@@ -24,10 +30,8 @@ const gettour = catchasync(async (req, res) => {
   });
 });
 const getloginform = catchasync(async (req, res) => {
-  res
-    .status(200)
-    .render('login', {
-      title: 'Log Into Your Account',
-    });
+  res.status(200).render('login', {
+    title: 'Log Into Your Account',
+  });
 });
-module.exports = { getoverview, gettour,getloginform };
+module.exports = { getoverview, gettour, getloginform };
