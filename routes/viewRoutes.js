1ee9453995fa468f3f125c7f0630db257f78a4bb
Himanshu <hv65616@gmail.com>
2023-11-23 23:29:19 +0530

    profile page route added

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const viewcontroller = require('../controller/viewController');
 const authcontroller = require('../controller/authController');
-router.use(authcontroller.isloggedin);
+// router.use(authcontroller.isloggedin);
 // Overview route to get all tour
-router.get('/overview', viewcontroller.getoverview);
+router.get('/overview', authcontroller.isloggedin,viewcontroller.getoverview);
 // Tour route to get particular tour
-// router.get('/tour/:slug', authcontroller.isloggedin, viewcontroller.gettour);
-router.get('/tour/:slug',viewcontroller.gettour);
-router.get('/login', viewcontroller.getloginform);
+router.get('/tour/:slug', authcontroller.isloggedin, viewcontroller.gettour);
+// router.get('/tour/:slug',viewcontroller.gettour);
+router.get('/login', authcontroller.isloggedin,viewcontroller.getloginform);
+// User Profile route
+router.get("/me",authcontroller.protect,viewcontroller.getaccount);
 module.exports = router;
