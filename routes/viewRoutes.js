4d9d4abc59c77b740da13323e31c62e8164f5d35
Himanshu <hv65616@gmail.com>
2023-11-25 22:47:07 +0530

    submit user data route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,11 +4,16 @@ const viewcontroller = require('../controller/viewController');
 const authcontroller = require('../controller/authController');
 // router.use(authcontroller.isloggedin);
 // Overview route to get all tour
-router.get('/overview', authcontroller.isloggedin,viewcontroller.getoverview);
+router.get('/overview', authcontroller.isloggedin, viewcontroller.getoverview);
 // Tour route to get particular tour
 router.get('/tour/:slug', authcontroller.isloggedin, viewcontroller.gettour);
 // router.get('/tour/:slug',viewcontroller.gettour);
-router.get('/login', authcontroller.isloggedin,viewcontroller.getloginform);
+router.get('/login', authcontroller.isloggedin, viewcontroller.getloginform);
 // User Profile route
-router.get("/me",authcontroller.protect,viewcontroller.getaccount);
+router.get('/me', authcontroller.protect, viewcontroller.getaccount);
+router.post(
+  '/submit-user-data',
+  authcontroller.protect,
+  viewcontroller.updateuserdata
+);
 module.exports = router;
