97c2da12f2bd39cd998bf9c84de383aafa359852
Himanshu <hv65616@gmail.com>
2023-12-15 14:40:31 +0530

    resize cover and body image and store tit

--- controller/tourController.js
+++ controller/tourController.js
@@ -40,10 +40,31 @@ const uploadtourphoto = upload.fields([
 ]);
 
 // resize tour image
-const resizetourimages = (req, res, next) => {
-  console.log(req.files);
+const resizetourimages = catchasync(async (req, res, next) => {
+  // console.log(req.files);
+  if (!req.files.imageCover || !req.files.images) return next();
+  // Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+  // Other image
+  req.body.images = [];
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+      req.body.images.push(filename);
+    })
+  );
   next();
-};
+});
 // created a middleware for functioning of endpoint named as top-5-cheaptours and in this middle ware we are passing default values for limit sort and fields
 const aliastoptours = (req, res, next) => {
   req.query.limit = '5';
