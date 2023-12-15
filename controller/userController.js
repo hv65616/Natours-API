f79fe0be32f346c404fc885bc37635e2fbbe5b09
Himanshu <hv65616@gmail.com>
2023-12-15 14:41:14 +0530

    making it async await

--- controller/userController.js
+++ controller/userController.js
@@ -34,16 +34,16 @@ const upload = multer({ storage: multerstorage, fileFilter: multerfilter });
 const uploaduserphoto = upload.single('photo');
 
 // middleware for resizing the user photo
-const resizeuserphoto = (req, res, next) => {
+const resizeuserphoto = catchasync(async (req, res, next) => {
   if (!req.file) return next();
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
   next();
-};
+});
 
 // this function is used to filter the data passed as req.body ans only consider those fields which are part of allowed fields it is pure javascript not nodejs
 const filterObj = (obj, ...allowedfields) => {
